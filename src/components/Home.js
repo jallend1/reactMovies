445e4c43166630b51f1f5a7b8290c7f8e84c7713
jallend1 <jallend1@gmail.com>
2020-08-12 14:44:15 -0700

    Extracts movie fetching into own custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,37 +13,12 @@ import MovieThumb from "./elements/MovieThumb";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
+//Custom Hook
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
 const Home = () => {
-  const [state, setState] = useState({ movies: [] });
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [{ state, loading, error }, fetchMoves] = useHomeFetch();
   console.log(state);
-
-  const fetchMovies = async (endpoint) => {
-    setError(false);
-    setLoading(true);
-    try {
-      const result = await (await fetch(endpoint)).json();
-      console.log(result);
-      setState((prev) => ({
-        ...prev,
-        movies: [...result.results],
-        heroImage: prev.heroImage || result.results[0],
-        currentPage: result.page,
-        totalPages: result.total_pages
-      }));
-    } catch (error) {
-      setError(true);
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
-  }, []);
-
   return (
     <>
       <HeroImage />
